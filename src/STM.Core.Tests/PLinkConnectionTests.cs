2827eb6754acb6ada985ee9e5fdbb15e89fdf5af
Stephan Burguchev <me@sburg.net>
2013-08-06 02:15:36 +0400

    some steps to plink connection test

---
+++ src/STM.Core.Tests/PLinkConnectionTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using STM.Core.Data;
+
+namespace STM.Core.Tests
+{
+    [TestFixture]
+    public class PLinkConnectionTests
+    {
+        [Test]
+        public void MethodName()
+        {
+            var connection = new ConnectionInfo
+                {
+                    Name = "SDF.org",
+                    HostName = "sdf.org",
+                    Port = 22,
+                    UserName = "stmut",
+                    Password = "test",
+                    //SharedSettings = new SharedConnectionSettings("default")
+                };
+            var connector = new PLinkConnection(connection);
+            connector.Open();
+
+        }
+    }
+}
